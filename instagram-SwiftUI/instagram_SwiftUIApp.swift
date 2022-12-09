f8d640cc817e06a02eab0884c0d758995e6c3b83
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2022-12-09 15:09:08 +0200

    Initial Commit

---
+++ instagram-SwiftUI/instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_SwiftUIApp.swift
+//  instagram-SwiftUI
+//
+//  Created by Ahmed Yamany on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
